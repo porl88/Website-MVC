da14385daafa09d8b5687beb021f3dddbe5c19a8
porl88 <xxx@xxx.com>
2016-10-30 16:14:26 +0100

    Email Controller and Views added - replaces .html email pages

---
+++ MVC.WebUI/Controllers/EmailController.cs
@@ -0,0 +1,21 @@
+﻿namespace MVC.WebUI.Controllers
+{
+    using System.Web.Mvc;
+
+    public class EmailController : Controller
+    {
+        // GET: /email/activateaccount
+        public ViewResult ActivateAccount(string token, string name)
+        {
+            ViewBag.Token = token;
+            ViewBag.Name = name;
+            return this.View();
+        }
+
+        // GET /email/password
+        public ViewResult Password()
+        {
+            return this.View();
+        }
+    }
+}

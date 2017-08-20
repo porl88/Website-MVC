d441426ed7e285c23a10bf38c96efab26698d913
porl88 <xxx@xxx.com>
2017-08-21 01:08:18 +0200

    LanguageRedirectAttribute rewritten

--- MVC.WebUI/Attributes/LanguageRedirectAttribute.cs
+++ MVC.WebUI/Attributes/LanguageRedirectAttribute.cs
@@ -4,60 +4,42 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
-    using System.Web.Routing;
-    using Ninject;
-    using Services.Culture;
-    using Services.Culture.Transfer;
 
     public class LanguageRedirectAttribute : ActionFilterAttribute
     {
-        [Inject]
-        public ILanguageService languageService;
-
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var lang = context.RouteData.Values["lang"] as string;
-
-            if (lang == null)
+            var request = context.HttpContext.Request;
+           
+            if (request.HttpMethod == "GET" && (request.UrlReferrer == null || !request.UrlReferrer.Host.Equals(request.Url.Host, StringComparison.InvariantCultureIgnoreCase)))
             {
-                var request = new GetPreferredLanguageRequest
-                {
-                    AvailableLanguages = new string[] { "en-gb", "fr-fr", "de-de" }
-                };
-
-                var preferredLang = this.languageService.GetPreferredLanguage(request);
-
-                var routes = new RouteValueDictionary
-                {
-                    { "lang", preferredLang },
-                    { "controller", context.RouteData.Values["controller"] },
-                    { "action", context.RouteData.Values["action"] },
-                    { "id", context.RouteData.Values["id"] }
-                };
-
+                var availableLanguages = new string[] { "en-gb", "fr-fr", "de-de" };
+                var preferredLang = this.GetPreferredLanguage(request, availableLanguages, "en-gb");
+                var routes = context.RouteData.Values;
+                routes["lang"] = preferredLang;
                 // 301 redirects are cached aggressively by browsers
                 context.HttpContext.Response.CacheControl = "no-cache";
                 context.Result = new RedirectToRouteResult("DefaultLocalized", routes, true);
             }
         }
 
-        //private string GetPreferredLanguage(HttpRequestBase request, string[] availableLanguages, string defaultLanguage)
-        //{
-        //    var browserLanguages = request.UserLanguages;
+        private string GetPreferredLanguage(HttpRequestBase request, string[] availableLanguages, string defaultLanguage)
+        {
+            var browserLanguages = request.UserLanguages;
 
-        //    foreach (var item in browserLanguages)
-        //    {
-        //        var lang = item.Split(';').First();
-        //        var langCode = availableLanguages.FirstOrDefault(x => x.Equals(lang, StringComparison.InvariantCultureIgnoreCase)) ??
-        //                       availableLanguages.FirstOrDefault(x => x.Split('-').First().Equals(lang.Split('-').First(), StringComparison.InvariantCultureIgnoreCase));
+            foreach (var item in browserLanguages)
+            {
+                var lang = item.Split(';').First();
+                var langCode = availableLanguages.FirstOrDefault(x => x.Equals(lang, StringComparison.InvariantCultureIgnoreCase)) ??
+                               availableLanguages.FirstOrDefault(x => x.Split('-').First().Equals(lang.Split('-').First(), StringComparison.InvariantCultureIgnoreCase));
 
-        //        if (!string.IsNullOrWhiteSpace(langCode))
-        //        {
-        //            return langCode;
-        //        }
-        //    }
+                if (!string.IsNullOrWhiteSpace(langCode))
+                {
+                    return langCode;
+                }
+            }
 
-        //    return defaultLanguage;
-        //}
+            return defaultLanguage;
+        }
     }
 }
